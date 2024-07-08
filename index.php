b064abe5a6d015c7603b1532057dfbb955b7047e
Avinash <avinashk8650@gmail.com>
2024-07-09 00:15:56 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="Assets/images/logo.png"/>
+    <title>IT Infotech - Software Solution</title>
+    <?php
+        require('includes/CSSlink.php');
+    ?>
+</head>
+
+<body class="index-page">
+    <?php
+        include('includes/header.php');
+    ?>
+    <?php
+        require('includes/JSlink.php');
+    ?>
+</body>
+</html>
